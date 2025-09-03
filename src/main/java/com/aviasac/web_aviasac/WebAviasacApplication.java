52caa04b9c0af126731c2fa6954c9a309d1982e3
ElvisRP22 <eremaycunarp544@gmail.com>
2025-09-03 12:12:59 -0500

    chore: subiendo proyecto inicial

---
+++ src/main/java/com/aviasac/web_aviasac/WebAviasacApplication.java
@@ -0,0 +1,13 @@
+package com.aviasac.web_aviasac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAviasacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAviasacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aviasac/web_aviasac/WebAviasacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviasac.web_aviasac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAviasacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
