48a14d41839826cc51edf09ee4c8c54d2441435c
ElvisRP22 <eremaycunarp544@gmail.com>
2025-09-29 21:49:21 -0500

    feat:Controller para admin

--- src/main/java/com/aviasac/web_aviasac/controller/TestimoniosController.java
+++ src/main/java/com/aviasac/web_aviasac/controller/TestimoniosController.java
@@ -1,13 +1,19 @@
 package com.aviasac.web_aviasac.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.aviasac.web_aviasac.model.Testimonio;
 
 @Controller
 public class TestimoniosController {
     
+    Testimonio testimonio = new Testimonio(1, "Juan Perez", "Excelente servicio, muy recomendado.");
+
     @GetMapping("/testimonios")
-    public String testimonios() {
+    public String testimonios(Model model) {
+        model.addAttribute("testimonio", testimonio);
         return "testimonios";
     }
 }
---
+++ src/main/java/com/aviasac/web_aviasac/model/Testimonio.java
@@ -0,0 +1,57 @@
+package com.aviasac.web_aviasac.model;
+
+public class Testimonio {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public Testimonio(int id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return String return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
