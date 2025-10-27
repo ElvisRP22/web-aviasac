1cc870980ae57b020d590f73bfec3755290f85f9
ElvisRP22 <eremaycunarp544@gmail.com>
2025-10-27 14:30:53 -0500

    feat(repository): agregando findByUsername y findByEmail

--- src/main/java/com/aviasac/web_aviasac/respository/UsuarioRepository.java
+++ src/main/java/com/aviasac/web_aviasac/respository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.aviasac.web_aviasac.respository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aviasac.web_aviasac.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-    
+    Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByEmail(String email);
 }
