3fe2024acd4d46aa4bc45bcccb593f527e0517ec
ElvisRP22 <eremaycunarp544@gmail.com>
2025-10-27 14:47:24 -0500

    feat(auth): implementa carga de usuario desde base de datos para login usando spring security

---
+++ src/main/java/com/aviasac/web_aviasac/services/impl/CustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package com.aviasac.web_aviasac.services.impl;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aviasac.web_aviasac.model.Rol;
+import com.aviasac.web_aviasac.model.Usuario;
+import com.aviasac.web_aviasac.respository.UsuarioRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        Collection<? extends GrantedAuthority> authorities = mapRolToAuthorities(usuario.getRol());
+
+        return new User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                authorities);
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolToAuthorities(Rol rol) {
+        return List.of(new SimpleGrantedAuthority(rol.getNombre()));
+    }
+
+}
