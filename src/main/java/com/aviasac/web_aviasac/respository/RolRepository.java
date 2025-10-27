d23c6cb4d572d21665271afe09e8c3385d44702a
ElvisRP22 <eremaycunarp544@gmail.com>
2025-10-27 14:30:03 -0500

    feat(repository): agregando metodo personalizado findByNombre

--- src/main/java/com/aviasac/web_aviasac/respository/RolRepository.java
+++ src/main/java/com/aviasac/web_aviasac/respository/RolRepository.java
@@ -1,9 +1,11 @@
 package com.aviasac.web_aviasac.respository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aviasac.web_aviasac.model.Rol;
 
 public interface RolRepository extends JpaRepository<Rol, Integer> {
-    
+    Optional<Rol> findByNombre(String nombre);
 }
