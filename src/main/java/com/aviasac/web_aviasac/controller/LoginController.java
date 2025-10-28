cbf59b20046a1ae99e8d137fdc24505e10d51487
ElvisRP22 <eremaycunarp544@gmail.com>
2025-10-28 12:34:28 -0500

    chore(removeUnsetImports): quitando importanciones no usadas

--- src/main/java/com/aviasac/web_aviasac/controller/LoginController.java
+++ src/main/java/com/aviasac/web_aviasac/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.aviasac.web_aviasac.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
