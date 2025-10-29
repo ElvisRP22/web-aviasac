da6c784cec49204ce26c79c635d60423be1026d8
ElvisRP22 <eremaycunarp544@gmail.com>
2025-10-29 18:21:48 -0500

    feat(existByEmail): agregando metodo personalizado en respository para verificar si hay un email suscrito

--- src/main/java/com/aviasac/web_aviasac/respository/SuscripcionRepository.java
+++ src/main/java/com/aviasac/web_aviasac/respository/SuscripcionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.aviasac.web_aviasac.model.Suscripcion;
 
 public interface SuscripcionRepository extends JpaRepository<Suscripcion, Integer> {
-    
+    boolean existsByEmail(String email);
 }
